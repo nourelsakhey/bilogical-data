36172eb308ed908f2efab0fae5c99d363d11abaf
nourelsakhey <36204564+nourelsakhey@users.noreply.github.com>
2018-12-10 21:37:53 +0200

    Update Condonstable.cpp

--- Condonstable.cpp
+++ Condonstable.cpp
@@ -1,54 +1,71 @@
-#include "Condonstable.h"
 
+#include"Condonstable.h"
 
-
-Condonstable::Condonstable()
+Codonstable::Codonstable()
 {
-	
 
 }
-void Condonstable::load_condons_from_file(string condons_file_name)
+Codonstable::~Codonstable()
 {
-	file.open(condons_file_name);
+
+}
+
+void Codonstable::load_codons_from_file(string codonsFileName)
+{
+	bool exit = true;
+	do {
+		try {
+			(file.is_open());
+			exit = true;
+		}
+		//ifstream file(codonsFileName);
+		//try {
+		//	file.is_open();
+		//}
+		catch (const ifstream::failure&e) {
+			cout << "the file is not found" << endl;
+			exit = false;
+		}
+	}
+	while (!exit);
 	while (!file.eof())
 	{
 
-
-		for (int i = 0;i < 64;i++) {
-			for (int j = 0;j < 3;j++) {
-				file >> condons[i].value[j];
+		for (int i = 0; i < 64; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				file >> codons[i].value[j];
 			}
-			file >> condons[i].aminoacid;
+			file >> codons[i].aminoacid;
 		}
-
-		
 	}
 }
-condon Condonstable::get_aminoacid(char*value)
+
+codon Codonstable::get_aminoacid(char * value)
 {
+	int count_ = 0, index = 0;
 	bool check = true;
-	for (int i = 0;i < 64;i++) {
-		for (int j = 0;j < 3;j++) {
-			if (condons[i].value[j] != value[j]) {
+	for (int i = 0; i < 64; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (codons[i].value[j] != value[j]) {
+
 				break;
 			}
-			
-			
-
+			else
+				continue;
+			count_++;
 		}
-		
+		if (count_ == 3)
+		{
+			break;
+			index = i;
+		}
 
 	}
-
-
-
+	return codons[index];
 }
-void Condonstable:: set_condon(char*value, char aminoacid, int index)
+void Codonstable::set_codon(char * value, char aminoacid, int index)
 {
 
-}
-
-Condonstable::~Condonstable()
-{
-	file.close();
 }
