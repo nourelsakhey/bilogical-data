8f5cf865b7deb9d0a0e2280e6968b8c69332e1e6
nourelsakhey <36204564+nourelsakhey@users.noreply.github.com>
2018-12-10 21:33:12 +0200

    Update Protein.h

--- Protein.h
+++ Protein.h
@@ -2,17 +2,27 @@
 #include"sequence.h"
 #include"DNA.h"
 #include"RNA.h"
+#include<iostream>
+using namespace std;
 
-enum Protein_type{hormon,enzyme,TE,cellular_function};
-class Protein:public sequence
+class DNA;
+
+enum Protein_Type{hormon,enzyme,TE,cellular_function};
+
+class Protein:public sequence 
 {
 private:
-	Protein_type type;
+	Protein_Type type;
 public:
 	Protein();
-	Protein(char*p);
-	DNA* get_DNAstrands_enoding_me(const DNA&bigDNA);
+	Protein(char *p, Protein_Type atype,int length);
+	//DNA* get_DNAstrands_enoding_me(const DNA&bigDNA);
 	void print();
+	Protein operator+(Protein &obj);
+	bool operator==(Protein &obj);
+	bool operator!=(Protein &obj);
+	friend ostream& operator<< (ostream& , Protein&);
+	friend istream& operator>> (istream&, Protein&);
 	~Protein();
 };
 
